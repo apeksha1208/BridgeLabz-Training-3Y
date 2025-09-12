5d4f61a599434fea7502c652ccc272f15965d53e
apeksha singh <apekshasingh2254@gmail.com>
2025-09-12 18:14:58 +0530

    completed

--- java-class/level-1/Circle.java
+++ java-class/level-1/Circle.java
@@ -11,12 +11,12 @@ public class Circle {
 	
 	//Creating method for calculating area of circle
 	public void areaCircle() {
-		System.out.printf("Area of circle: %.4f",(Math.PI*radius*radius));
+		System.out.println("Area of circle: %.4f",(Math.PI*radius*radius));
 	}
 	
 	//Creating method for calculating circumference of circle
 	public void circumferenceCircle() {
-		System.out.printf("\nCircumference of circle: %.4f ",(2*Math.PI*radius));
+		System.out.println("\nCircumference of circle: %.4f ",(2*Math.PI*radius));
 	}
 	
 	public static void main(String[] args) {
